4e8c4c5daf126479bcc4a49114168e5f49bf1c89
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-05-31 19:01:02 +0530

    init

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,47 +1,48 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 type Props = {
-    activeItem:number
-}
-const navItems =[
-    {
-        title: 'Home',
-        href:'/',
-    },{
-        title: 'Marketplace',
-        href:'/market',
-    },
-    {
-        title: 'About Us',
-        href:'/about',
-    },
-    {
-        title: 'Contact Us',
-        href:'/contact',
-    },
-    {
-        title:"Policy",
-        href:'/policy',
-    }
-]
+  activeItem: number;
+};
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Marketplace",
+    href: "/market",
+  },
+  {
+    title: "About Us",
+    href: "/about",
+  },
+  {
+    title: "Contact Us",
+    href: "/contact",
+  },
+  {
+    title: "Policy",
+    href: "/policy",
+  },
+];
 
 const Navigation = ({ activeItem }: Props) => {
-    return (
-      <div className="block md:flex">
-        {navItems.map((item, index) => (
-          <Link key={item.title} href={item.href}>
-            <h5
-              className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${
-                activeItem === index && "text-[#C4EF17]"
-              }`}
-            >
-              {item.title}
-            </h5>
-          </Link>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div className="block md:flex">
+      {navItems.map((item, index) => (
+        <Link key={item.title} href={item.href}>
+          <h5
+            className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${
+              activeItem === index && "text-[#C4EF17]"
+            }`}
+          >
+            {item.title}
+          </h5>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
